df749a80c0692b926d0a32ca008ee3ca791d0745
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2023-09-24 11:57:03 +0900

    ㅂㅈ
    고새앻ㅆ어

---
+++ Assets/Script/Click.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour
+{
+    public Image How;
+    public void Good()
+    {
+        How.gameObject.SetActive(true);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
